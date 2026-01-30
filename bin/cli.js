9795d49f31cbe62837b309f8818ec0d7b3970735
Cruz_Noah <noahsantosdacruz@gmail.com>
2026-01-31 00:09:51 +0100

    Ajout gestion des arguments --help et --version

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
 import '../src/index.js';
+
+
+const args = process.argv.slice(2);
+
+if (args.includes('--help') || args.includes('-h')) {
+  console.log(`
+CLI_NOJO - Mon outil en ligne de commande
+
+Usage:
+  cli-nojo [options]
+
+Options:
+  --help, -h     Affiche cette aide
+  --version, -v  Affiche la version
+  `);
+  process.exit(0);
+}
+
+if (args.includes('--version') || args.includes('-v')) {
+  console.log('CLI_NOJO version 1.0.0');
+  process.exit(0);
+}
+
+console.log('Hello from CLI_NOJO!');
+console.log('Arguments reçus:', args);
